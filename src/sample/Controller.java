8eae7f6004537582a98b0ea80c3a9d417a27bd46
Shpions <xxiii@ukr.net>
2015-03-04 23:36:12 +0200

    first_commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    @FXML
+    Button delete;
+    @FXML
+    Button create;
+    @FXML
+    Button watch;
+    @FXML
+    Button search;
+
+
+
+    @FXML
+    public void createAction(){
+
+    }
+}
